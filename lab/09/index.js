4b58bc47e05c8854aaeb3c1ec59127b376d71b39
Tunchanok Patana-anake <Tunchanok@Meow.wireless.newschool.edu>
2018-03-30 09:04:47 -0400

    [some message]

---
+++ lab/09/index.js
@@ -0,0 +1,8 @@
+
+//select an element
+var box = document.querySelector('#box')
+//add a listener
+box.addEventListener('scroll',function (event){
+//do some stuff
+console.log(event)
+})
---
+++ studio/02-TOILET-PAPER-MAGAZINE/assets/js/main.js
@@ -0,0 +1,4 @@
+function change (picurl, picurl2) {
+document.picture1.src = picurl;
+document.picture2.src = picurl2;
+}
