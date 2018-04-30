fe1fdb762db3c8b1e572a5a4c6bed4915e46e315
Tunchanok Patana-anake <Tunchanok@Meow.wireless.newschool.edu>
2018-04-30 14:30:08 -0400

    [some message]

--- studio/behance/assets/js/main.js
+++ studio/behance/assets/js/main.js
@@ -1,21 +1,31 @@
 //https://www.behance.net/v2/projects/?q=photography%20graphic&client_id=RbEgyxFDwBZlOmDvWqayuwejUt3fADvb
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://www.behance.net/v2/projects/?q=photography%20graphic&client_id=RbEgyxFDwBZlOmDvWqayuwejUt3fADvb', true);
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://www.behance.net/v2/projects/?q=photography%20graphic&client_id=RbEgyxFDwBZlOmDvWqayuwejUt3fADvb', true);
 
-request.onload = function () {
+// request.onload = function () {
+// console.log('hi')
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response);
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
+//   if (request.status >= 200 && request.status < 400) {
+//    console.log(data)
+//   } else {
+//     console.log('error');
+//   }
+// }
 
-  if (request.status >= 200 && request.status < 400) {
-   console.log(data)
-  } else {
-    console.log('error');
-  }
+// request.send();
+$("button").click(function(){
+    $.ajax({url: "https://www.behance.net/v2/projects/?q=photography%20graphic&client_id=RbEgyxFDwBZlOmDvWqayuwejUt3fADvb", success: function(result){
+      console.log('hi')
+        $("#div1").html(result);
+    },
+error: function(e){
+  console.log(e)
 }
-
-request.send();
+           });
+});
