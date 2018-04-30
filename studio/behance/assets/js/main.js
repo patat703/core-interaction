1cbbd730dbc575bd659282511e0816355b8fe241
Tunchanok Patana-anake <Tunchanok@Meow.wireless.newschool.edu>
2018-04-30 14:47:34 -0400

    [some message]

--- studio/behance/assets/js/main.js
+++ studio/behance/assets/js/main.js
@@ -17,7 +17,8 @@
 //     console.log('error');
 //   }
 // }
-
+//result.projects[0].covers.original
+//result.projects[0].name
 // request.send();
 $("button").click(function(){
     $.ajax({type: "GET",
@@ -25,7 +26,22 @@ $("button").click(function(){
   url: "https://www.behance.net/v2/projects/?q=photography%20graphic&client_id=RbEgyxFDwBZlOmDvWqayuwejUt3fADvb",
   success: function(result){
       console.log(result.projects)
-      debugger
+      for (var i = 0; i < result.projects.length; i++) {
+        var name = result.projects[i].name
+        var imgLink = result.projects[i].covers.original
+        //create div to hold each image and title
+        var container = $('<div/>');
+        var img = $('<img/>');
+        var title = $('<h1/>');
+        $(img).attr('src', imgLink);
+        $(title).html(name);
+        $(container).append(img);
+        $(container).append(title);
+        $(body).append(container);
+
+
+        
+      }
         // $("#div1").html(result);
     },
 error: function(e){
